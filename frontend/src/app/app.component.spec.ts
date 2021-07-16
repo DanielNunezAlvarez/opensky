6125115a576aeba7e8c0b93eb9d223dcdbad79d6
DanielNunezAlvarez <87385425+DanielNunezAlvarez@users.noreply.github.com>
2021-07-16 11:05:56 +0200

    Update app.component.spec.ts

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -20,7 +20,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ocp-dashboard'`, () => {
+  it(`should have as title 'opensky'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('ocp-dashboard');
