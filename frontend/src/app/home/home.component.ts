83eb7fbf03bd3025f69d9eb31de3a3b8fd7f6903
DanielNunezAlvarez <87385425+DanielNunezAlvarez@users.noreply.github.com>
2021-07-17 14:20:18 +0200

    Update home.component.ts

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -143,15 +143,6 @@ export class HomeComponent {
         visible: true, 
         dataSource: [
               { latitude: 37.6276571, longitude: -122.4276688, name: 'San Bruno' }, 
-              { latitude: 33.5302186, longitude: -117.7418381, name: 'Laguna Niguel' }, 
-              { latitude: 40.7424509, longitude: -74.0081468, name: 'New York' }, 
-              { latitude: -23.5268201, longitude: -46.6489927, name: 'Bom Retiro' }, 
-              { latitude: 43.6533855, longitude: -79.3729994, name: 'Toronto' }, 
-              { latitude: 48.8773406, longitude: 2.3299627, name: 'Paris' }, 
-              { latitude: 52.4643089, longitude: 13.4107368, name: 'Berlin' }, 
-              { latitude: 19.1555762, longitude: 72.8849595, name: 'Mumbai' }, 
-              { latitude: 35.6628744, longitude: 139.7345469, name: 'Minato' }, 
-              { latitude: 51.5326602, longitude: -0.1262422, name: 'London' }
           ], 
         shape: 'Image', 
         imageUrl: './assets/images/arrivalPosition.jpg', 
@@ -161,11 +152,11 @@ export class HomeComponent {
       }
         
         ];
-        this.maps.layers[0].navigationLineSettings = [];
-        this.navigationLines = [];
-        this.emptySavedLinePositions();
+//         this.maps.layers[0].navigationLineSettings = [];
+//         this.navigationLines = [];
+//         this.emptySavedLinePositions();
         this.maps.refresh();
-        this.disableButton = true;
+//         this.disableButton = true;
 
 
     this.airportFlag = true;
