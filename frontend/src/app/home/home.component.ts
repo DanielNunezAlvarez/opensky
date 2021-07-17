6ffcf26b892ba2f3dae82fa83a75918817d22dff
DanielNunezAlvarez <87385425+DanielNunezAlvarez@users.noreply.github.com>
2021-07-17 14:18:55 +0200

    Update home.component.ts

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -138,7 +138,29 @@ export class HomeComponent {
    
 //       this.getOpenskyStatistics(this.airport, this.begin, this.end)
     
-        this.maps.layers[0].markerSettings = [];
+        this.maps.layers[0].markerSettings = [
+              { 
+        visible: true, 
+        dataSource: [
+              { latitude: 37.6276571, longitude: -122.4276688, name: 'San Bruno' }, 
+              { latitude: 33.5302186, longitude: -117.7418381, name: 'Laguna Niguel' }, 
+              { latitude: 40.7424509, longitude: -74.0081468, name: 'New York' }, 
+              { latitude: -23.5268201, longitude: -46.6489927, name: 'Bom Retiro' }, 
+              { latitude: 43.6533855, longitude: -79.3729994, name: 'Toronto' }, 
+              { latitude: 48.8773406, longitude: 2.3299627, name: 'Paris' }, 
+              { latitude: 52.4643089, longitude: 13.4107368, name: 'Berlin' }, 
+              { latitude: 19.1555762, longitude: 72.8849595, name: 'Mumbai' }, 
+              { latitude: 35.6628744, longitude: 139.7345469, name: 'Minato' }, 
+              { latitude: 51.5326602, longitude: -0.1262422, name: 'London' }
+          ], 
+        shape: 'Image', 
+        imageUrl: './assets/images/arrivalPosition.jpg', 
+        height: 20, width: 20,
+        tooltipSettings: { visible: true, valuePath: 'name' }, 
+        animationDuration: 0 
+      }
+        
+        ];
         this.maps.layers[0].navigationLineSettings = [];
         this.navigationLines = [];
         this.emptySavedLinePositions();
