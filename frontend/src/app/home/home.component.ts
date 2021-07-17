92cdde2820e179f75b6277776aad40d28c7a0a75
DanielNunezAlvarez <87385425+DanielNunezAlvarez@users.noreply.github.com>
2021-07-17 10:25:58 +0200

    Update home.component.ts

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -34,7 +34,7 @@ export class HomeComponent {
   public airport: string = 'EDDM';
   public begin: string = '1517227200';
   public end: string = '1517230800';
-  public arrivalLocation = airportDatabase['EDDM'];
+  public arrivalLocation = airportsDatabase['EDDM'];
   public loadingTable = false;
   public pageSettings: Object;
   public today: Date = new Date();
